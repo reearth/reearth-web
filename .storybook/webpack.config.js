1d1ad6a124708b1310fee84fd67ebc7ffb016208
rot1024 <aayhrot@gmail.com>
2021-07-06 21:19:39 +0900

    simplify webpack config of storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,12 @@
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
-const fs = require("fs");
 
 module.exports = ({ config }) => {
   config.externals = {
     ...config.externals,
     cesium: "Cesium",
   };
-
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    use: [
-      {
-        loader: "babel-loader",
-        options: JSON.parse(fs.readFileSync(path.resolve(__dirname, "../.babelrc"))),
-      },
-    ],
-  });
 
   config.module.rules.push({
     test: /\.yml$/,
@@ -41,8 +29,6 @@ module.exports = ({ config }) => {
       CESIUM_BASE_URL: JSON.stringify("cesium"),
     }),
   );
-
-  config.resolve.extensions.push(".ts", ".tsx");
 
   config.resolve.alias = {
     ...config.resolve.alias,
