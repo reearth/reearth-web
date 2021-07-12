dcaf27dd9678222059905ad0129c0cd1ee2afe3a
rot1024 <aayhrot@gmail.com>
2021-07-12 12:25:14 +0900

    fix styled

--- src/theme/styled.ts
+++ src/theme/styled.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ThemeContext } from "@emotion/react";
-import rawStyled from "@emotion/styled/macro";
+import rawStyled from "@emotion/styled";
 export { css, keyframes } from "@emotion/react";
 
 import { Theme } from "./theme";
