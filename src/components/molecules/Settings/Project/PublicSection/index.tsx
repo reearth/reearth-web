a9e74f1b18972706feed71ed14f8cf9aaef7813f
KaWaite <flippindaisy@gmail.com>
2021-07-16 15:32:13 +0900

    Revert theme color name that doesn't exist yet

--- src/components/molecules/Settings/Project/PublicSection/index.tsx
+++ src/components/molecules/Settings/Project/PublicSection/index.tsx
@@ -76,7 +76,7 @@ const PublicSection: React.FC<Props> = ({
 };
 
 const Wrapper = styled.div`
-  background-color: ${props => props.theme.main.lighterBg};
+  background-color: ${props => props.theme.colors.bg[3]};
 `;
 
 export default PublicSection;
