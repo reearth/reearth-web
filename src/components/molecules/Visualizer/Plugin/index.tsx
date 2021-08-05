b23ba9616543fc3fbc9ad55ef3c6ff75a3001e73
rot1024 <aayhrot@gmail.com>
2021-08-05 12:14:35 +0900

    enable plugins in visualizer

--- src/components/molecules/Visualizer/Plugin/index.tsx
+++ src/components/molecules/Visualizer/Plugin/index.tsx
@@ -1,11 +1,10 @@
-import { CSSProperties } from "react";
-
-import { Primitive, Widget, Block } from "@reearth/plugin";
-// import P from "@reearth/components/atoms/Plugin";
-
-// import useHooks from "./hooks";
+import React, { CSSProperties } from "react";
 
 export type { Primitive, Block, Widget } from "@reearth/plugin";
+import { Primitive, Widget, Block } from "@reearth/plugin";
+import P from "@reearth/components/atoms/Plugin";
+
+import useHooks from "./hooks";
 
 export type Props = {
   className?: string;
@@ -23,51 +22,50 @@ export type Props = {
   block?: Block;
 };
 
-// const defaultPluginBaseUrl = `${window.REEARTH_CONFIG?.api || "/api"}/plugins`;
+export default function Plugin({
+  className,
+  style,
+  sourceCode,
+  pluginId,
+  extensionId,
+  extensionType,
+  property,
+  visible,
+  pluginBaseUrl = "/plugins",
+  primitive,
+  widget,
+  block,
+  sceneProperty,
+}: Props): JSX.Element | null {
+  const { skip, src, exposed, isMarshalable, staticExposed, handleError, handleMessage } = useHooks(
+    {
+      pluginId,
+      extensionId,
+      sourceCode,
+      extensionType,
+      property,
+      pluginBaseUrl,
+      primitive,
+      widget,
+      block,
+      sceneProperty,
+    },
+  );
 
-// className,
-// style,
-// sourceCode,
-// pluginId,
-// extensionId,
-// extensionType,
-// property,
-// visible,
-// pluginBaseUrl = defaultPluginBaseUrl,
-// primitive,
-// widget,
-// block,
-// sceneProperty,
-export default function Plugin(_: Props): JSX.Element | null {
-  // const { skip, src, exposed, isMarshalable, staticExposed, handleError, handleMessage } = useHooks(
-  //   {
-  //     pluginId,
-  //     extensionId,
-  //     sourceCode,
-  //     extensionType,
-  //     property,
-  //     pluginBaseUrl,
-  //     primitive,
-  //     widget,
-  //     block,
-  //     sceneProperty,
-  //   },
-  // );
-
-  // return !skip && (src || sourceCode) ? (
-  //   <P
-  //     className={className}
-  //     style={style}
-  //     src={src}
-  //     sourceCode={sourceCode}
-  //     isMarshalable={isMarshalable}
-  //     staticExposed={staticExposed}
-  //     exposed={exposed}
-  //     onError={handleError}
-  //     onMessage={handleMessage}
-  //     canBeVisible={visible}
-  //   />
-  // ) : null;
+  return !skip && (src || sourceCode) ? (
+    <P
+      className={className}
+      style={style}
+      src={src}
+      sourceCode={sourceCode}
+      isMarshalable={isMarshalable}
+      staticExposed={staticExposed}
+      exposed={exposed}
+      onError={handleError}
+      onMessage={handleMessage}
+      canBeVisible={visible}
+    />
+  ) : null;
 
   return null;
 }
