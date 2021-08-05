b4f23b170a3b658099a69acad3a386f7a69b022a
rot1024 <aayhrot@gmail.com>
2021-08-05 19:10:40 +0900

    add lookAt to storybook cesium component

--- src/components/molecules/Visualizer/Engine/Cesium/storybook.tsx
+++ src/components/molecules/Visualizer/Engine/Cesium/storybook.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { CameraFlyTo } from "resium";
-import { Cartesian3 } from "cesium";
+import { CameraFlyTo, CameraFlyToBoundingSphere } from "resium";
+import { BoundingSphere, Cartesian3 } from "cesium";
 import { action } from "@storybook/addon-actions";
 
 import { Provider } from "../../context";
@@ -10,21 +10,36 @@ import CesiumEngine from ".";
 export const location = { lat: 35.652832, lng: 139.839478, height: 1000 };
 
 // For storybook
-export const V: React.FC<{ location?: { lat: number; lng: number } }> = ({
-  children,
-  location: l = location,
-}) => {
+export const V: React.FC<{
+  location?: { lat: number; lng: number };
+  lookAt?: { lat: number; lng: number; height: number; range: number };
+}> = ({ children, location: l = location, lookAt }) => {
   return (
     <Provider value={context}>
       <CesiumEngine
         ready
-        property={{ tiles: [{ id: "default", tile_type: "default" }] }}
+        property={{
+          tiles: [{ id: "default", tile_type: "default" }],
+        }}
         onPrimitiveSelect={action("Cesium: onLayerSelect")}>
-        <CameraFlyTo
-          destination={Cartesian3.fromDegrees((l ?? location).lng, (l ?? location).lat, 10000)}
-          duration={0}
-          once
-        />
+        {lookAt ? (
+          <CameraFlyToBoundingSphere
+            boundingSphere={
+              new BoundingSphere(
+                Cartesian3.fromDegrees(lookAt.lng, lookAt.lat, lookAt.height),
+                lookAt.range,
+              )
+            }
+            duration={0}
+            once
+          />
+        ) : (
+          <CameraFlyTo
+            destination={Cartesian3.fromDegrees((l ?? location).lng, (l ?? location).lat, 10000)}
+            duration={0}
+            once
+          />
+        )}
         {children}
       </CesiumEngine>
     </Provider>
