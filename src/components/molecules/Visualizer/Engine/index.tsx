6404b16076f49e805f834a898c99b0ffeaba0c98
rot1024 <aayhrot@gmail.com>
2021-08-05 21:58:04 +0900

    support animation and shadows

--- src/components/molecules/Visualizer/Engine/index.tsx
+++ src/components/molecules/Visualizer/Engine/index.tsx
@@ -16,6 +16,9 @@ export type SceneProperty = {
   default?: {
     camera?: Camera;
     terrain?: boolean;
+    terrainExaggeration?: number; // default: 1
+    terrainExaggerationRelativeHeight?: number; // default: 0
+    depthTestAgainstTerrain?: boolean;
     skybox?: boolean;
     bgcolor?: string;
     ion?: string;
@@ -28,6 +31,7 @@ export type SceneProperty = {
     tile_minLevel?: number;
   }[];
   atmosphere?: {
+    enable_shadows?: boolean;
     enable_sun?: boolean;
     enable_lighting?: boolean;
     ground_atmosphere?: boolean;
@@ -37,6 +41,9 @@ export type SceneProperty = {
     brightness_shift?: number;
     hue_shift?: number;
     surturation_shift?: number;
+  };
+  timeline?: {
+    animation?: boolean;
   };
   googleAnalytics?: {
     enableGA?: boolean;
