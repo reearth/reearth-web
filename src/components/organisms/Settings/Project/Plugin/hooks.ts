2efeacb40cb7a0656e474162ac86fc5857fa5c22
HideBa <baba.papa1120.ba@gmail.com>
2021-08-05 15:15:05 +0900

    delete comment

--- src/components/organisms/Settings/Project/Plugin/hooks.ts
+++ src/components/organisms/Settings/Project/Plugin/hooks.ts
@@ -28,9 +28,6 @@ export default (projectId: string) => {
     skip: !projectId,
   });
 
-  // const installedPluginIds = useMemo(() => {
-  //   return rawSceneData?.scene?.plugins.map(p => p.plugin?.id);
-  // }, [rawSceneData?.scene?.plugins]);
   const installedPlugins = useMemo(() => {
     return rawSceneData
       ? rawSceneData?.scene?.plugins
@@ -45,18 +42,6 @@ export default (projectId: string) => {
           }))
       : [];
   }, [rawSceneData]);
-
-  // const plugins = useMemo((): PluginItem[] => {
-  //   return rawPluginsData
-  //     ? rawPluginsData?.installablePlugins.map<PluginItem>(p => ({
-  //         title: p.name,
-  //         bodyMarkdown: p.description,
-  //         author: p.author,
-  //         thumbnailUrl: p.thumbnailUrl,
-  //         isInstalled: !!installedPluginIds?.includes(p.name), //TODO: After back-end decide how to generate plugin's id, fix here.
-  //       }))
-  //     : [];
-  // }, [installedPluginIds, rawPluginsData]);
 
   const installByUploadingZipFile = useCallback(
     async (files: FileList) => {
