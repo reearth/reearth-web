75b213a261126cce9cc766d5389386bda1693591
rot1024 <aayhrot@gmail.com>
2021-08-09 22:08:50 +0900

    fix type error

--- src/util/use-delayed-count.test.ts
+++ src/util/use-delayed-count.test.ts
@@ -8,49 +8,73 @@ test("normal", async () => {
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(undefined);
 
-  act(() => result.current[2](true));
+  act(() => {
+    result.current[2](true);
+  });
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(undefined);
 
-  act(() => result.current[2](false));
+  act(() => {
+    result.current[2](false);
+  });
   expect(result.current[0]).toBe(1);
   expect(result.current[1]).toBe(0);
 
-  act(() => jest.advanceTimersByTime(100));
+  act(() => {
+    jest.advanceTimersByTime(100);
+  });
   expect(result.current[0]).toBe(2);
   expect(result.current[1]).toBe(1);
 
-  act(() => jest.advanceTimersByTime(200));
+  act(() => {
+    jest.advanceTimersByTime(200);
+  });
   expect(result.current[0]).toBe(3);
   expect(result.current[1]).toBe(2);
 
-  act(() => jest.advanceTimersByTime(300));
+  act(() => {
+    jest.advanceTimersByTime(300);
+  });
   expect(result.current[0]).toBe(4);
   expect(result.current[1]).toBe(3);
-  act(() => jest.advanceTimersByTime(100));
+  act(() => {
+    jest.advanceTimersByTime(100);
+  });
   expect(result.current[0]).toBe(4);
   expect(result.current[1]).toBe(3);
 
-  act(() => result.current[2](false));
+  act(() => {
+    result.current[2](false);
+  });
   expect(result.current[0]).toBe(4);
   expect(result.current[1]).toBe(3);
 
-  act(() => result.current[2](true));
+  act(() => {
+    result.current[2](true);
+  });
   expect(result.current[0]).toBe(3);
   expect(result.current[1]).toBe(4);
 
-  act(() => jest.advanceTimersByTime(300));
+  act(() => {
+    jest.advanceTimersByTime(300);
+  });
   expect(result.current[0]).toBe(2);
   expect(result.current[1]).toBe(3);
 
-  act(() => jest.advanceTimersByTime(200));
+  act(() => {
+    jest.advanceTimersByTime(200);
+  });
   expect(result.current[0]).toBe(1);
   expect(result.current[1]).toBe(2);
 
-  act(() => jest.advanceTimersByTime(100));
+  act(() => {
+    jest.advanceTimersByTime(100);
+  });
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(1);
-  act(() => jest.advanceTimersByTime(100));
+  act(() => {
+    jest.advanceTimersByTime(100);
+  });
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(1);
 });
@@ -62,22 +86,32 @@ test("different durations", async () => {
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(undefined);
 
-  act(() => result.current[2](false));
+  act(() => {
+    result.current[2](false);
+  });
   expect(result.current[0]).toBe(1);
   expect(result.current[1]).toBe(0);
 
-  act(() => jest.advanceTimersByTime(100));
+  act(() => {
+    jest.advanceTimersByTime(100);
+  });
   expect(result.current[0]).toBe(2);
   expect(result.current[1]).toBe(1);
 
-  act(() => result.current[2](true));
+  act(() => {
+    result.current[2](true);
+  });
   expect(result.current[0]).toBe(1);
   expect(result.current[1]).toBe(2);
 
-  act(() => jest.advanceTimersByTime(100));
+  act(() => {
+    jest.advanceTimersByTime(100);
+  });
   expect(result.current[0]).toBe(1);
   expect(result.current[1]).toBe(2);
-  act(() => jest.advanceTimersByTime(200));
+  act(() => {
+    jest.advanceTimersByTime(200);
+  });
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(1);
 });
@@ -89,11 +123,15 @@ test("skip", async () => {
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(undefined);
 
-  act(() => result.current[2](false, true));
+  act(() => {
+    result.current[2](false, true);
+  });
   expect(result.current[0]).toBe(4);
   expect(result.current[1]).toBe(0);
 
-  act(() => result.current[2](true, true));
+  act(() => {
+    result.current[2](true, true);
+  });
   expect(result.current[0]).toBe(0);
   expect(result.current[1]).toBe(4);
 });
