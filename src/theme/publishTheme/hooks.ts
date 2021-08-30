93856535e81c6bf1ed8966a4fe223f214c1c413c
basel.issmail <basel.issmail@gmail.com>
2021-08-30 11:44:32 +0300

    Fix typo

--- src/theme/publishTheme/hooks.ts
+++ src/theme/publishTheme/hooks.ts
@@ -14,7 +14,7 @@ export type ReTheme = {
 export default (seneThemeOptions?: ReTheme) => {
   const [publishedTheme, setPublishedTheme] = useState<PublishTheme>(dark);
   function addAlpha(color?: string, opacity?: number): string {
-    // coerce values so ti is between 0 and 1.
+    // coerce values so it is between 0 and 1.
     const _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
     return color + _opacity.toString(16).toUpperCase();
   }
