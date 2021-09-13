585766d06206827a35e176d122e2b70c54e9fa1d
KaWaite <flippindaisy@gmail.com>
2021-09-13 17:35:13 +0900

    Add unit tests

---
+++ src/components/molecules/Common/Notification/index.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from "@reearth/test/utils";
+import NotificationBanner, { NotificationType } from "./index";
+
+const sampleNotification: NotificationType = {
+  type: "info",
+  heading: "Notice",
+  text: "This is a notification for something super cool.",
+};
+
+test("Notification component should be rendered", () => {
+  render(<NotificationBanner />);
+});
+
+test("Notification component should display notification heading", () => {
+  render(<NotificationBanner visible notification={sampleNotification} />);
+  expect(screen.getByText(/Notice/)).toBeInTheDocument();
+});
+
+test("Notification component should display notification text", () => {
+  render(<NotificationBanner visible notification={sampleNotification} />);
+  expect(screen.getByText(/This is a notification/)).toBeInTheDocument();
+});
+
+test("Notification component should not display anything", () => {
+  render(<NotificationBanner notification={sampleNotification} />);
+  expect(screen.getByText(/This is a notification/)).not.toBeVisible();
+});
