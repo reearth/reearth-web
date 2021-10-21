845be2cd9f277c05342b55795e76f14083d2f1ca
KaWaite <flippindaisy@gmail.com>
2021-10-21 11:32:32 +0900

    move rootpage logic to organisms

--- src/components/pages/Authentication/RootPage/index.tsx
+++ src/components/pages/Authentication/RootPage/index.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 
-import Loading from "@reearth/components/atoms/Loading";
-import MoleculeRootPage from "@reearth/components/molecules/RootPage";
-
-import useHooks from "./hooks";
+import OrganismRootPage from "@reearth/components/organisms/Authentication/RootPage";
 
 export type Props = {
   path?: string;
 };
 
 const RootPage: React.FC<Props> = () => {
-  const { isLoading, isAuthenticated } = useHooks();
-
-  return isLoading ? <Loading /> : !isAuthenticated ? <MoleculeRootPage /> : null;
+  return <OrganismRootPage />;
 };
 
 export default RootPage;
