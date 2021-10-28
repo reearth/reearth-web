073d3f819a32c2eaf4c84cdea7abfa965de33bfa
KaWaite <flippindaisy@gmail.com>
2021-10-28 11:53:55 +0900

    fix: add wrong field to test

--- src/config.test.ts
+++ src/config.test.ts
@@ -4,15 +4,17 @@ test("convert password policy to regular expression", () => {
   const passwordPolicy = {
     whiteSpace: "(?=.*\\s)",
     highSecurity: "^(?=.*[a-z])(?=.*[A-Z])((?=(.*\\d){2}))",
+    wrong: "@[",
   };
   const actual = convertPasswordPolicy(passwordPolicy);
   expect(actual).toStrictEqual({
     whiteSpace: /(?=.*\s)/,
     highSecurity: /^(?=.*[a-z])(?=.*[A-Z])((?=(.*\d){2}))/,
+    wrong: undefined,
   });
 });
 
 test("return undefined if no password policy", () => {
   const actual = convertPasswordPolicy(undefined);
-  expect(actual).toEqual(undefined);
+  expect(actual).toBeUndefined();
 });
