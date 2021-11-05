ea51c866b831088fb6274d17c408143a9cac8406
KaWaite <kyle.waite@outlook.com>
2021-11-05 10:39:22 +0900

    Add password policy/check to signup

--- src/components/molecules/Authentication/Signup/index.tsx
+++ src/components/molecules/Authentication/Signup/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@reach/router";
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useIntl } from "react-intl";
 
 import Button from "@reearth/components/atoms/Button";
@@ -27,8 +27,11 @@ export type Props = {
 const Signup: React.FC<Props> = ({ onSignup, passwordPolicy }) => {
   const intl = useIntl();
   const theme = useTheme();
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [regexMessage, setRegexMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [sent, setSent] = useState(false);
 
   const handleUsernameInput = useCallback(
     (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -40,73 +43,181 @@ const Signup: React.FC<Props> = ({ onSignup, passwordPolicy }) => {
 
   const handlePasswordInput = useCallback(
     (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      const newValue = e.currentTarget.value;
-      setPassword(newValue);
+      const password = e.currentTarget.value;
+      setPassword(password);
+      switch (true) {
+        case passwordPolicy?.whitespace?.test(password):
+          setRegexMessage(
+            intl.formatMessage({
+              defaultMessage: "No whitespace is allowed.",
+            }),
+          );
+          break;
+        case passwordPolicy?.tooShort?.test(password):
+          setRegexMessage(
+            intl.formatMessage({
+              defaultMessage: "Too short.",
+            }),
+          );
+          break;
+        case passwordPolicy?.tooLong?.test(password):
+          setRegexMessage(
+            intl.formatMessage({
+              defaultMessage: "That is terribly long.",
+            }),
+          );
+          break;
+        case passwordPolicy?.highSecurity?.test(password):
+          setRegexMessage(intl.formatMessage({ defaultMessage: "That password is great!" }));
+          break;
+        case passwordPolicy?.medSecurity?.test(password):
+          setRegexMessage(intl.formatMessage({ defaultMessage: "That password is better." }));
+          break;
+        case passwordPolicy?.lowSecurity?.test(password):
+          setRegexMessage(intl.formatMessage({ defaultMessage: "That password is okay." }));
+          break;
+        default:
+          setRegexMessage(
+            intl.formatMessage({
+              defaultMessage: "That password confuses me, but might be okay.",
+            }),
+          );
+          break;
+      }
     },
-    [],
+    [password], // eslint-disable-line react-hooks/exhaustive-deps
   );
 
   const handleSignup = useCallback(() => {
     onSignup(username, password);
+    setSent(true);
   }, [username, password, onSignup]);
-  console.log(passwordPolicy, "pp");
+
+  useEffect(() => {
+    if (
+      (passwordPolicy?.highSecurity && !passwordPolicy.highSecurity.test(password)) ||
+      passwordPolicy?.tooShort?.test(password) ||
+      passwordPolicy?.tooLong?.test(password)
+    ) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [password, passwordPolicy]);
 
   return (
     <AuthPage>
-      <Icon className="form-item" icon="logoColorful" size={60} />
-      <Text className="form-item" size="l" customColor>
-        {intl.formatMessage({ defaultMessage: "Create your Account" })}
-      </Text>
-      <StyledInput
-        className="form-item"
-        placeholder={intl.formatMessage({ defaultMessage: "Username or email" })}
-        color={theme.main.weak}
-        value={username}
-        autoFocus
-        onChange={handleUsernameInput}
-      />
-      <StyledInput
-        className="form-item"
-        placeholder={intl.formatMessage({ defaultMessage: "Password" })}
-        type="password"
-        autoComplete="new-password"
-        color={theme.main.weak}
-        value={password}
-        onChange={handlePasswordInput}
-      />
-      <StyledButton
-        className="form-item"
-        large
-        onClick={handleSignup}
-        text={intl.formatMessage({ defaultMessage: "Continue" })}
-      />
-      <Footer className="form-item">
-        <Text size="xs" color={theme.main.weak}>
-          {intl.formatMessage({ defaultMessage: "Already have an account?" })}
-        </Text>
-        <StyledLink to={"/login"}>
-          <Text
-            size="xs"
-            color={theme.main.link}
-            weight="bold"
-            otherProperties={{ marginLeft: "6px" }}>
-            {intl.formatMessage({ defaultMessage: "Log in" })}
+      {sent ? (
+        <SentFormWrapper direction="column" align="center" justify="center">
+          <SentForm direction="column" align="center" justify="space-between">
+            <Icon icon="mailCircle" color={theme.colors.brand.blue.strongest} />
+            <Text size="l" customColor>
+              {intl.formatMessage({ defaultMessage: "Check Your Email" })}
+            </Text>
+            <Text size="m" customColor>
+              {intl.formatMessage({
+                defaultMessage:
+                  "Please check your inbox for instructions on how to verify your account.",
+              })}
+            </Text>
+            <StyledButton
+              className="form-item"
+              large
+              onClick={handleSignup}
+              border
+              color={theme.main.weak}
+              background={theme.other.white}
+              text={intl.formatMessage({ defaultMessage: "Resend email" })}
+            />
+          </SentForm>
+          <StyledLink to={"/login"}>
+            <Text
+              size="xs"
+              color={theme.main.link}
+              weight="bold"
+              otherProperties={{ marginLeft: "6px" }}>
+              {intl.formatMessage({ defaultMessage: "Go to log in page." })}
+            </Text>
+          </StyledLink>
+        </SentFormWrapper>
+      ) : (
+        <>
+          <Icon className="form-item" icon="logoColorful" size={60} />
+          <Text className="form-item" size="l" customColor>
+            {intl.formatMessage({ defaultMessage: "Create your Account" })}
           </Text>
-        </StyledLink>
-      </Footer>
+          <StyledInput
+            className="form-item"
+            placeholder={intl.formatMessage({ defaultMessage: "Email address" })}
+            color={theme.main.weak}
+            value={username}
+            autoFocus
+            onChange={handleUsernameInput}
+          />
+          <PasswordWrapper direction="column">
+            <StyledInput
+              className="form-item"
+              placeholder={intl.formatMessage({ defaultMessage: "Password" })}
+              type="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={handlePasswordInput}
+              color={
+                passwordPolicy?.tooLong?.test(password)
+                  ? theme.main.danger
+                  : passwordPolicy?.highSecurity?.test(password)
+                  ? theme.main.accent
+                  : theme.main.weak
+              }
+            />
+            <PasswordMessage size="xs" customColor>
+              {password ? regexMessage : undefined}
+            </PasswordMessage>
+          </PasswordWrapper>
+          <StyledButton
+            className="form-item"
+            large
+            onClick={handleSignup}
+            disabled={disabled}
+            color={disabled ? theme.main.text : theme.other.white}
+            background={disabled ? theme.main.weak : theme.main.link}
+            text={intl.formatMessage({ defaultMessage: "Continue" })}
+          />
+          <Footer className="form-item">
+            <Text size="xs" color={theme.main.weak}>
+              {intl.formatMessage({ defaultMessage: "Already have an account?" })}
+            </Text>
+            <StyledLink to={"/login"}>
+              <Text
+                size="xs"
+                color={theme.main.link}
+                weight="bold"
+                otherProperties={{ marginLeft: "6px" }}>
+                {intl.formatMessage({ defaultMessage: "Log in" })}
+              </Text>
+            </StyledLink>
+          </Footer>
+        </>
+      )}
     </AuthPage>
   );
 };
 
-const StyledButton = styled(Button)`
+const StyledButton = styled(Button)<{ color?: string; background?: string; border?: boolean }>`
   width: 100%;
-  background: ${({ theme }) => theme.main.link};
-  border: none;
+  background: ${({ background }) => background};
+  border: ${({ border, theme }) => (border ? `1px solid ${theme.main.borderStrong}` : "none")};
   border-radius: 2px;
-  color: ${({ theme }) => theme.other.white};
+  color: ${({ color }) => color};
+  margin-top: 30px;
+
+  :hover {
+    color: ${({ color }) => color};
+    background: ${({ background }) => background};
+  }
 `;
 
-const StyledInput = styled.input`
+const StyledInput = styled.input<{ color?: string }>`
   width: 100%;
   box-sizing: border-box;
   -webkit-box-sizing: border-box;
@@ -116,15 +227,35 @@ const StyledInput = styled.input`
   border-radius: 3px;
   padding: ${metricsSizes.s}px;
   outline: none;
+  ${({ color }) => color && `color: ${color};`}
 
   :focus {
-    border: 2px solid ${({ theme }) => theme.main.link};
-    margin: -1px -1px 23px -1px;
+    border: 1px solid ${({ theme }) => theme.main.link};
   }
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+`;
+
+const PasswordWrapper = styled(Flex)`
+  width: 100%;
+  color: ${({ theme }) => theme.text.pale};
+`;
+
+const PasswordMessage = styled(Text)`
+  margin-left: ${metricsSizes.m}px;
+  margin-top: ${metricsSizes["2xs"]}px;
+  font-style: italic;
+`;
+
+const SentFormWrapper = styled(Flex)`
+  width: 100%;
+  height: 400px;
+`;
+
+const SentForm = styled(Flex)`
+  height: 250px;
 `;
 
 const Footer = styled(Flex)`
