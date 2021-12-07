3ed431b0fe2d1c891d0786420e51bd2618b22b85
basel.issmail <basel.issmail@gmail.com>
2021-12-07 08:12:14 +0200

    Remove default values

--- src/components/molecules/Visualizer/Engine/Cesium/hooks.ts
+++ src/components/molecules/Visualizer/Engine/Cesium/hooks.ts
@@ -255,13 +255,7 @@ export default ({
       !geodsic
     )
       return undefined;
-    // TODO: default values to fill
-    if (!property.cameraLimiter.target_width) {
-      property.cameraLimiter.target_width = 1000000;
-    }
-    if (!property.cameraLimiter.target_height) {
-      property.cameraLimiter.target_height = 1000000;
-    }
+
     const topDemention = geodsic.geodesicVertical.interpolateUsingSurfaceDistance(
       property.cameraLimiter.target_height / 2,
     );
@@ -329,14 +323,6 @@ export default ({
     const computedViewRectangle = camera.computeViewRectangle() as Rectangle;
     const rectangleHalfWidth = Rectangle.computeWidth(computedViewRectangle) * Math.PI * 1000000;
     const rectangleHalfHeight = Rectangle.computeHeight(computedViewRectangle) * Math.PI * 1000000;
-
-    // TODO: default values to fill
-    if (!property.cameraLimiter.target_width) {
-      property.cameraLimiter.target_width = 1000000;
-    }
-    if (!property.cameraLimiter.target_height) {
-      property.cameraLimiter.target_height = 1000000;
-    }
 
     const recTopDemention = geodsic.geodesicVertical.interpolateUsingSurfaceDistance(
       property.cameraLimiter.target_height / 2 + rectangleHalfHeight,
