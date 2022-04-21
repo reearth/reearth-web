05c351b71b61f10fc17d05d552ca5ac9417fe67f
KaWaite <kyle.waite@outlook.com>
2022-04-21 13:50:59 +0900

    singularized file names, moved primitive q/m into scene

--- src/gql/queries/scene.ts
+++ src/gql/queries/scene.ts
@@ -1,5 +1,7 @@
 import { gql } from "@apollo/client";
 
+import { layerFragment } from "@reearth/gql/fragments";
+
 export const SCENE = gql`
   query Scene($projectId: ID!) {
     scene(projectId: $projectId) {
@@ -29,4 +31,62 @@ export const CREATE_SCENE = gql`
       }
     }
   }
+`;
+
+export const GET_PRIMITIVES = gql`
+  query GetPrimitives($sceneId: ID!, $lang: Lang) {
+    node(id: $sceneId, type: SCENE) {
+      id
+      ... on Scene {
+        plugins {
+          plugin {
+            id
+            extensions {
+              extensionId
+              translatedDescription(lang: $lang)
+              translatedName(lang: $lang)
+              icon
+              type
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_LAYER_ITEM_FROM_PRIMITIVE = gql`
+  mutation addLayerItemFromPrimitive(
+    $parentLayerId: ID!
+    $pluginId: ID!
+    $extensionId: ID!
+    $name: String
+    $lat: Float
+    $lng: Float
+    $index: Int
+    $lang: Lang
+  ) {
+    addLayerItem(
+      input: {
+        parentLayerId: $parentLayerId
+        pluginId: $pluginId
+        extensionId: $extensionId
+        name: $name
+        lat: $lat
+        lng: $lng
+        index: $index
+      }
+    ) {
+      parentLayer {
+        id
+        ...Layer3Fragment
+      }
+      layer {
+        id
+        ...LayerFragment
+      }
+    }
+  }
+
+  ${layerFragment}
 `;
