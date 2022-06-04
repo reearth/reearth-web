24ffaa9c13ff98e7e60dc3a41e1db544af269823
keiya01 <keiya.s.0210@gmail.com>
2022-06-04 18:12:11 +0900

    fix: ignore notation in i18next

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -2,4 +2,7 @@ module.exports = {
   locales: ["en", "ja"],
   output: "src/i18n/translations/$LOCALE.yml",
   input: ["src/**/*.{ts,tsx}"],
+  // allow keys to be phrases having `:`, `.`
+  namespaceSeparator: false,
+  keySeparator: false,
 };
