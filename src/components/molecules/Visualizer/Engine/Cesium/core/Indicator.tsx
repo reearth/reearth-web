baf5dcc14ea0a1e82f63e8124b9f6299e04d1853
rot1024 <aayhrot@gmail.com>
2022-06-06 18:04:53 +0900

    impl indicator

--- src/components/molecules/Visualizer/Engine/Cesium/core/Indicator.tsx
+++ src/components/molecules/Visualizer/Engine/Cesium/core/Indicator.tsx
@@ -1,3 +1,4 @@
+import useTransition, { TransitionStatus } from "@rot1024/use-transition";
 import { BoundingSphere, Cartesian2, Cartesian3, SceneTransforms } from "cesium";
 import React, { useEffect, useState } from "react";
 import { useCesium } from "resium";
@@ -11,6 +12,10 @@ export type Props = {
 export default function Indicator({ className }: Props): JSX.Element | null {
   const { viewer } = useCesium();
   const [pos, setPos] = useState<Cartesian2>();
+  const transiton = useTransition(!!pos, 1000, {
+    mountOnEnter: true,
+    unmountOnExit: true,
+  });
 
   useEffect(() => {
     if (!viewer) return;
@@ -57,13 +62,22 @@ export default function Indicator({ className }: Props): JSX.Element | null {
     };
   }, [viewer]);
 
-  return pos ? <I className={className} style={{ left: pos.x + "px", top: pos.y + "px" }} /> : null;
+  return transiton !== "unmounted" && pos ? (
+    <I
+      className={className}
+      transiton={transiton}
+      style={{ left: pos.x + "px", top: pos.y + "px" }}
+    />
+  ) : null;
 }
 
-const I = styled.div`
+const I = styled.div<{ transiton: TransitionStatus }>`
   position: absolute;
   width: 50px;
   height: 50px;
   background-color: red;
   transform: translate(-50%, -50%);
+  transition: ${({ transiton }) =>
+    transiton === "entering" || transiton === "exiting" ? "all 1s ease" : ""};
+  opacity: ${({ transiton }) => (transiton === "entering" || transiton === "entered" ? 1 : 0)};
 `;
