f88cbda9c0305a35846aa8af164c5cbfa3242bac
keiya01 <keiya.s.0210@gmail.com>
2022-06-26 21:46:03 +0900

    test: fix storybook

--- src/components/atoms/Timeline/index.stories.tsx
+++ src/components/atoms/Timeline/index.stories.tsx
@@ -12,11 +12,20 @@ export default {
 export const Normal: Story<Props> = () => (
   <Timeline
     currentTime={new Date("2022-06-30T12:20:00.000").getTime()}
-    timelineRange={[
-      new Date("2022-06-30T00:00:00.000").getTime(),
-      new Date("2022-07-03T12:21:21.221").getTime(),
-    ]}
-    iconWidth={25}
-    renderIcon={() => <Icon icon="ellipse" alt="ellipse" size={25} />}
+    range={{
+      start: new Date("2022-06-30T21:00:00.000").getTime(),
+      end: new Date("2022-07-03T12:21:21.221").getTime(),
+    }}
+    knobSize={25}
+    renderKnob={() => <Icon icon="ellipse" alt="ellipse" size={25} />}
+  />
+);
+
+export const DefaultRange: Story<Props> = () => (
+  <Timeline
+    // Forward a hour
+    currentTime={Date.now() + 3600000}
+    knobSize={25}
+    renderKnob={() => <Icon icon="ellipse" alt="ellipse" size={25} />}
   />
 );
