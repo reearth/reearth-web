bda0dbd4fe6b6cee623198b23c1921172baa85de
keiya01 <keiya.s.0210@gmail.com>
2022-07-04 20:56:00 +0900

    feat: add interaction for timeline

--- src/components/atoms/Timeline/index.tsx
+++ src/components/atoms/Timeline/index.tsx
@@ -1,12 +1,24 @@
 import { memo, ReactElement, useMemo } from "react";
 
-import { metricsSizes, styled } from "@reearth/theme";
+import { styled } from "@reearth/theme";
 import { XXSRegular } from "@reearth/theme/fonts";
 
-type Range = {
-  start: number;
-  end: number;
-};
+import {
+  BORDER_WIDTH,
+  DAY_SECS,
+  EPOCH_SEC,
+  GAP_HORIZONTAL,
+  HOURS_SECS,
+  MAX_ZOOM_RATIO,
+  MEMORY_INTERVAL,
+  MEMORY_ZOOM_INTERVAL,
+  MINUTES_SEC,
+  NORMAL_MEMORY_WIDTH,
+  PADDING_HORIZONTAL,
+  STRONG_MEMORY_WIDTH,
+} from "./constants";
+import { useTimelineInteraction } from "./hooks";
+import { Range, TimeEventHandler } from "./types";
 
 export type Props = {
   /**
@@ -21,17 +33,9 @@ export type Props = {
   range?: { [K in keyof Range]?: Range[K] };
   renderKnob: () => ReactElement;
   knobSize: number;
+  onClick?: TimeEventHandler;
+  onDrag?: TimeEventHandler;
 };
-
-const EPOCH_SEC = 1000;
-const PADDING_HORIZONTAL = metricsSizes["m"];
-const STRONG_MEMORY_HOURS = 3;
-const STRONG_MEMORY_WIDTH = 2;
-const NORMAL_MEMORY_WIDTH = 1;
-const GAP_HORIZONTAL = 14;
-const HOURS_SECS = 3600;
-const MEMORY_INTERVAL = 600;
-const DAY_SECS = 86400;
 
 const getRange = (range: Props["range"]): Range => {
   const { start, end } = range || {};
@@ -62,7 +66,14 @@ const getRange = (range: Props["range"]): Range => {
 };
 
 const Timeline: React.FC<Props> = memo(
-  function TimelinePresenter({ currentTime, range: _range, renderKnob, knobSize }) {
+  function TimelinePresenter({
+    currentTime,
+    range: _range,
+    renderKnob,
+    knobSize,
+    onClick,
+    onDrag,
+  }) {
     const range = useMemo(() => {
       const range = getRange(_range);
       if (process.env.NODE_ENV !== "production") {
@@ -73,33 +84,57 @@ const Timeline: React.FC<Props> = memo(
       return range;
     }, [_range]);
     const { start, end } = range;
+    const { zoom, events } = useTimelineInteraction({ range, onClick, onDrag });
+    const gapHorizontal = GAP_HORIZONTAL * (zoom - Math.trunc(zoom) + 1);
+    const memoryInterval = Math.max(
+      MEMORY_INTERVAL - Math.trunc(zoom - 1) * MEMORY_ZOOM_INTERVAL * MINUTES_SEC,
+      MINUTES_SEC,
+    );
+    const strongMemoryHours = MAX_ZOOM_RATIO - Math.trunc(zoom) + 1;
     const epochDiff = end - start;
 
     // convert epoch diff to second.
     const memoryCount = useMemo(
-      () => Math.trunc(epochDiff / EPOCH_SEC / MEMORY_INTERVAL),
-      [epochDiff],
+      () => Math.trunc(epochDiff / EPOCH_SEC / memoryInterval),
+      [epochDiff, memoryInterval],
     );
 
     const startDate = useMemo(() => new Date(start), [start]);
-    const hoursCount = Math.trunc(HOURS_SECS / MEMORY_INTERVAL);
+    const hoursCount = Math.trunc(HOURS_SECS / memoryInterval);
     // Convert memory count to pixel.
     const currentPosition = useMemo(() => {
-      const diff = Math.min((currentTime - start) / EPOCH_SEC / MEMORY_INTERVAL, memoryCount);
-      const strongMemoryCount = diff / (hoursCount * STRONG_MEMORY_HOURS);
+      const diff = Math.min((currentTime - start) / EPOCH_SEC / memoryInterval, memoryCount);
+      const strongMemoryCount = diff / (hoursCount * strongMemoryHours);
       return Math.max(
-        diff * GAP_HORIZONTAL +
+        diff * gapHorizontal +
           (diff - strongMemoryCount) * NORMAL_MEMORY_WIDTH +
           strongMemoryCount * STRONG_MEMORY_WIDTH +
           STRONG_MEMORY_WIDTH / 2,
         0,
       );
-    }, [currentTime, start, memoryCount, hoursCount]);
+    }, [
+      currentTime,
+      start,
+      memoryCount,
+      hoursCount,
+      gapHorizontal,
+      memoryInterval,
+      strongMemoryHours,
+    ]);
 
     return (
       <Container>
-        <MemoryBox>
-          <MemoryList start={startDate} memoryCount={memoryCount} hoursCount={hoursCount} />
+        <MemoryBox {...events}>
+          <MemoryContainer style={{ gap: `0 ${gapHorizontal}px` }}>
+            <MemoryList
+              start={startDate}
+              memoryCount={memoryCount}
+              hoursCount={hoursCount}
+              gapHorizontal={gapHorizontal}
+              memoryInterval={memoryInterval}
+              strongMemoryHours={strongMemoryHours}
+            />
+          </MemoryContainer>
           <Icon
             style={{
               left: currentPosition + PADDING_HORIZONTAL - knobSize / 2,
@@ -132,22 +167,32 @@ type MemoryListProps = {
   start: Date;
   memoryCount: number;
   hoursCount: number;
+  gapHorizontal: number;
+  memoryInterval: number;
+  strongMemoryHours: number;
 };
 
 const MemoryList: React.FC<MemoryListProps> = memo(
-  function MemoryListPresenter({ start, memoryCount, hoursCount }) {
+  function MemoryListPresenter({
+    start,
+    memoryCount,
+    hoursCount,
+    memoryInterval,
+    strongMemoryHours,
+  }) {
+    const strongHours = hoursCount * strongMemoryHours;
+    const lastStrongMemoryIdx = strongHours * (memoryCount / strongHours);
     return (
-      <MemoryContainer>
+      <>
         {[...Array(memoryCount + 1)].map((_, idx) => {
           const isHour = idx % hoursCount === 0;
-          const isStrongMemory = idx % (hoursCount * STRONG_MEMORY_HOURS) === 0;
-          if (isStrongMemory) {
-            const d = new Date(start.getTime() + idx * EPOCH_SEC * MEMORY_INTERVAL);
+          const isStrongMemory = idx % strongHours === 0;
+          if (isStrongMemory && idx !== lastStrongMemoryIdx) {
+            const d = new Date(start.getTime() + idx * EPOCH_SEC * memoryInterval);
             const year = d.getFullYear();
             const month = MONTH_LABEL_LIST[d.getMonth()];
             const date = `${d.getDate()}`.padStart(2, "0");
             const hour = `${d.getHours()}`.padStart(2, "0");
-            // TODO: Fix date format
             const label = `${month} ${date} ${year} ${hour}:00:00.0000`;
 
             return (
@@ -159,10 +204,14 @@ const MemoryList: React.FC<MemoryListProps> = memo(
           }
           return <Memory key={`${idx}`} isHour={isHour} isStrongMemory={isStrongMemory} />;
         })}
-      </MemoryContainer>
+      </>
     );
   },
-  (prev, next) => prev.memoryCount === next.memoryCount && prev.start === next.start,
+  (prev, next) =>
+    prev.memoryCount === next.memoryCount &&
+    prev.start === next.start &&
+    prev.memoryInterval === next.memoryInterval &&
+    prev.strongMemoryHours === next.strongMemoryHours,
 );
 
 const Container = styled.div`
@@ -170,12 +219,16 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  padding: ${({ theme }) => `${theme.metrics.m} ${theme.metrics.xs}`};
+  padding: ${({ theme }) => `${theme.metrics.m}px ${theme.metrics.xs}px`};
   box-sizing: border-box;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 `;
 
 const MemoryBox = styled.div`
-  border: 1px solid ${({ theme }) => theme.colors.publish.dark.icon.weak};
+  border: ${BORDER_WIDTH}px solid ${({ theme }) => theme.colors.publish.dark.icon.weak};
   border-radius: 5px;
   box-sizing: border-box;
   position: relative;
@@ -202,10 +255,10 @@ const Icon = styled.div`
 
 const MemoryContainer = styled.div`
   display: flex;
-  gap: 0 ${GAP_HORIZONTAL}px;
   min-width: 100%;
   height: 30px;
   align-items: flex-end;
+  will-change: auto;
   padding-left: ${PADDING_HORIZONTAL}px;
   ::after {
     content: "";
