c80dd513df6eb61323f8457b6d31f38ee82f8d03
rot1024 <aayhrot@gmail.com>
2022-07-25 20:49:07 +0900

    fix raf test

--- src/util/raf.test.ts
+++ src/util/raf.test.ts
@@ -3,32 +3,32 @@ import { describe, expect, test, vi } from "vitest";
 import { interval, intervalDuring, tweenInterval } from "./raf";
 
 describe("interval", () => {
-  // TODO: Modify the test so that it always succeeds.
-  // test("regular", done => {
-  //   const re  questAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
-  //   const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
+  test("regular", () => {
+    vi.useFakeTimers();
+    const requestAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
+    const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
 
-  //   let count = 0;
-  //   const cb = vi.fn(() => ++count <= 3);
-  //   interval(cb);
+    let count = 0;
+    const cb = vi.fn<[number], boolean>(() => ++count <= 3);
+    interval(cb);
 
-  //   expect(cb).toBeCalledTimes(0);
-  //   expect(requestAnimationFrame).toBeCalledTimes(1);
-  //   expect(cancelAnimationFrame).toBeCalledTimes(0);
+    expect(cb).toBeCalledTimes(0);
+    expect(requestAnimationFrame).toBeCalledTimes(1);
+    expect(cancelAnimationFrame).toBeCalledTimes(0);
 
-  //   window.setTimeout(() => {
-  //     expect(cb).toBeCalledTimes(4);
-  //     expect(cb.mock.calls[0][0]).toBe(0);
-  //     for (let i = 1; i < cb.mock.calls.length; i++) {
-  //       expect(cb.mock.calls[i][0]).toBeGreaterThan(cb.mock.calls[i - 1][0]);
-  //     }
-  //     expect(requestAnimationFrame).toBeCalledTimes(4);
-  //     expect(cancelAnimationFrame).toBeCalledTimes(0);
-  //     done();
-  //   }, 100);
-  // });
+    vi.advanceTimersByTime(100);
 
-  test("cancel", done => {
+    expect(cb).toBeCalledTimes(4);
+    expect(cb.mock.calls[0][0]).toBe(0);
+    for (let i = 1; i < cb.mock.calls.length; i++) {
+      expect(cb.mock.calls[i][0]).toBeGreaterThan(cb.mock.calls[i - 1][0]);
+    }
+    expect(requestAnimationFrame).toBeCalledTimes(4);
+    expect(cancelAnimationFrame).toBeCalledTimes(0);
+  });
+
+  test("cancel", () => {
+    vi.useFakeTimers();
     const requestAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
     const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
 
@@ -45,10 +45,9 @@ describe("interval", () => {
     const requestId = requestAnimationFrame.mock.results[0].value;
     expect(cancelAnimationFrame).toBeCalledWith(requestId);
 
-    window.setTimeout(() => {
-      expect(cb).toBeCalledTimes(0);
-      done();
-    }, 50);
+    vi.advanceTimersByTime(50);
+
+    expect(cb).toBeCalledTimes(0);
   });
 
   test("delay", () => {
@@ -74,66 +73,62 @@ describe("interval", () => {
   });
 });
 
-describe("intervalDuring", () => {
-  test("regular", done => {
-    const requestAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
-    const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
-    const cb = vi.fn();
-    intervalDuring(cb, 100);
+test("intervalDuring", () => {
+  vi.useFakeTimers();
+  const requestAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
+  const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
+  const cb = vi.fn();
+  intervalDuring(cb, 100);
 
-    expect(cb).toBeCalledTimes(0);
-    expect(requestAnimationFrame).toBeCalledTimes(1);
-    expect(cancelAnimationFrame).toBeCalledTimes(0);
+  expect(cb).toBeCalledTimes(0);
+  expect(requestAnimationFrame).toBeCalledTimes(1);
+  expect(cancelAnimationFrame).toBeCalledTimes(0);
 
-    window.setTimeout(() => {
-      const times = cb.mock.calls.length;
-      expect(requestAnimationFrame).toBeCalledTimes(times);
-      expect(cancelAnimationFrame).toBeCalledTimes(0);
+  vi.advanceTimersByTime(200);
 
-      expect(cb.mock.calls[0][0]).toBe(0);
-      expect(cb.mock.calls[cb.mock.calls.length - 1][0]).toBe(1);
-      for (let i = 1; i < cb.mock.calls.length; i++) {
-        expect(cb.mock.calls[i][0]).toBeGreaterThan(cb.mock.calls[i - 1][0]);
-      }
+  const times = cb.mock.calls.length;
+  expect(requestAnimationFrame).toBeCalledTimes(times);
+  expect(cancelAnimationFrame).toBeCalledTimes(0);
 
-      window.setTimeout(() => {
-        // cb will not be called after intervalDuring ends
-        expect(cb).toBeCalledTimes(times);
-        done();
-      }, 100);
-    }, 200);
-  });
+  expect(cb.mock.calls[0][0]).toBe(0);
+  expect(cb.mock.calls[cb.mock.calls.length - 1][0]).toBe(1);
+  for (let i = 1; i < cb.mock.calls.length; i++) {
+    expect(cb.mock.calls[i][0]).toBeGreaterThan(cb.mock.calls[i - 1][0]);
+  }
+
+  vi.advanceTimersByTime(100);
+
+  // cb will not be called after intervalDuring ends
+  expect(cb).toBeCalledTimes(times);
 });
 
-describe("tweenInterval", () => {
-  test("regular", done => {
-    const requestAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
-    const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
-    const cb = vi.fn();
-    tweenInterval(cb, "cubic", 100);
+test("tweenInterval", () => {
+  vi.useFakeTimers();
+  const requestAnimationFrame = vi.spyOn(window, "requestAnimationFrame");
+  const cancelAnimationFrame = vi.spyOn(window, "cancelAnimationFrame");
+  const cb = vi.fn();
+  tweenInterval(cb, "cubic", 100);
 
-    expect(cb).toBeCalledTimes(0);
-    expect(requestAnimationFrame).toBeCalledTimes(1);
-    expect(cancelAnimationFrame).toBeCalledTimes(0);
+  expect(cb).toBeCalledTimes(0);
+  expect(requestAnimationFrame).toBeCalledTimes(1);
+  expect(cancelAnimationFrame).toBeCalledTimes(0);
 
-    window.setTimeout(() => {
-      const times = cb.mock.calls.length;
-      expect(requestAnimationFrame).toBeCalledTimes(times);
-      expect(cancelAnimationFrame).toBeCalledTimes(0);
+  vi.advanceTimersByTime(200);
 
-      expect(cb.mock.calls[0][0]).toBe(0);
-      expect(cb.mock.calls[0][1]).toBe(0);
-      expect(cb.mock.calls[cb.mock.calls.length - 1][0]).toBe(1);
-      expect(cb.mock.calls[cb.mock.calls.length - 1][1]).toBe(1);
-      for (let i = 1; i < cb.mock.calls.length; i++) {
-        expect(cb.mock.calls[i][1]).toBeGreaterThan(cb.mock.calls[i - 1][1]);
-      }
+  const times = cb.mock.calls.length;
+  expect(requestAnimationFrame).toBeCalledTimes(times);
+  expect(cancelAnimationFrame).toBeCalledTimes(0);
 
-      window.setTimeout(() => {
-        // cb will not be called after intervalDuring ends
-        expect(cb).toBeCalledTimes(times);
-        done();
-      }, 100);
-    }, 200);
-  });
+  expect(cb.mock.calls[0][0]).toBe(0);
+  expect(cb.mock.calls[0][1]).toBe(0);
+  expect(cb.mock.calls[cb.mock.calls.length - 1][0]).toBe(1);
+  expect(cb.mock.calls[cb.mock.calls.length - 1][1]).toBe(1);
+  for (let i = 1; i < cb.mock.calls.length; i++) {
+    expect(cb.mock.calls[i][1]).toBeGreaterThan(cb.mock.calls[i - 1][1]);
+  }
+
+  vi.advanceTimersByTime(100);
+
+  // cb will not be called after intervalDuring ends
+  expect(cb).toBeCalledTimes(times);
 });
