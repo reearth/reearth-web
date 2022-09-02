a68009e7375007862aa19d6830c1007db34d0e5b
rot1024 <aayhrot@gmail.com>
2022-09-02 19:04:12 +0900

    test cacheAtom

--- src/components/molecules/Visualizer/next/atoms.ts
+++ src/components/molecules/Visualizer/next/atoms.ts
@@ -1,10 +1,10 @@
 import { atom } from "jotai";
-import { atomFamily } from "jotai/utils";
 
 import { fetchData } from "./data";
 import type { Layer, Feature, Data } from "./types";
+import { cacheAtom } from "./utils";
 
-const globalDataCache = dataCacheAtom();
+const globalFeatureCache = cacheAtom<Feature[]>();
 
 export function layerAtom() {
   const layer = atom<Layer | undefined>(undefined);
@@ -13,67 +13,44 @@ export function layerAtom() {
   const features = atom(get => {
     const l = get(layer);
     if (!l?.data) return;
-    return get(data.getter)?.get(dataKey(l.data));
+    return get(data.getFeatures)?.get(dataKey(l.data));
   });
 
   const writeLayer = atom(null, (_get, set, value: Layer | undefined) => {
     set(layer, value);
     if (value?.data) {
-      set(data.fetch, value.data);
+      set(data.setAndFetchData, value.data);
     }
   });
 
   return {
     features,
     writeLayer,
-    writeFeatures: data.write,
+    writeFeatures: data.setFeatures,
   };
 }
 
-function dataAtom({ map, cache } = globalDataCache) {
-  const fetch = atom(null, async (get, set, value: Data) => {
+function dataAtom([getCache, setCache] = globalFeatureCache) {
+  const setAndFetchData = atom(null, async (get, set, value: Data) => {
     const key = dataKey(value);
-    const a = cache(key);
-    const cached = get(a);
+    const cached = get(getCache)?.get(key);
     if (cached) return;
 
     const data = await fetchData(value);
     if (data) {
-      set(a, data);
+      set(setCache, { key, value: data });
     }
   });
 
-  const write = atom(null, async (get, set, value: { key: string; features: Feature[] }) => {
-    set(cache(value.key), value.features);
+  const setFeatures = atom(null, async (_get, set, value: { key: string; features: Feature[] }) => {
+    set(setCache, { key: value.key, value: value.features });
   });
 
   return {
-    getter: map,
-    write,
-    fetch,
+    getFeatures: getCache,
+    setFeatures,
+    setAndFetchData,
   };
-}
-
-function dataCacheAtom() {
-  const map = atom<Map<string, Feature[]> | undefined>(undefined);
-  const cache = atomFamily((key: string) =>
-    atom(
-      get => get(map)?.get(key),
-      (get, set, value: Feature[]) => {
-        const m = get(map) ?? new Map();
-        if (!value) {
-          m.delete(key);
-        } else {
-          m.set(key, value);
-        }
-        set(map, m);
-      },
-    ),
-  );
-
-  const getter = atom(get => get(map));
-
-  return { map: getter, cache };
 }
 
 function dataKey(data: Data): string {
--- src/components/molecules/Visualizer/next/utils.ts
+++ src/components/molecules/Visualizer/next/utils.ts
@@ -1,5 +1,23 @@
+import { atom } from "jotai";
+
 import type { TreeLayer, Layer } from "./types";
 
 export function flattenLayers(layers: TreeLayer[] | undefined): Layer[] | undefined {
   return layers?.flatMap(l => ("children" in l ? flattenLayers(l.children) ?? [] : [l]));
+}
+
+export function cacheAtom<T>() {
+  const map = atom<Map<string, T> | undefined>(undefined);
+  const get = atom(get => get(map));
+  const set = atom(null, (get, set, value: { key: string; value?: T }) => {
+    const m = get(map) ?? new Map();
+    if (typeof value.value === "undefined") {
+      m.delete(value.key);
+    } else {
+      m.set(value.key, value.value);
+    }
+    set(map, m);
+  });
+
+  return [get, set] as const;
 }
