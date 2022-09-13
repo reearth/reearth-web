0369495c9764b6897eb2ce4edb89b5c3d1829956
nina992 <nouralali992@gmail.com>
2022-09-13 04:49:35 +0300

    osm

--- src/components/molecules/Visualizer/Engine/Cesium/Tileset/index.tsx
+++ src/components/molecules/Visualizer/Engine/Cesium/Tileset/index.tsx
@@ -1,4 +1,8 @@
-import { Cesium3DTileset as Cesium3DTilesetType, Cesium3DTileStyle } from "cesium";
+import {
+  Cesium3DTileset as Cesium3DTilesetType,
+  Cesium3DTileStyle,
+  createOsmBuildings,
+} from "cesium";
 import { useCallback, useEffect, useState } from "react";
 import { Cesium3DTileset, CesiumComponentRef, useCesium } from "resium";
 
@@ -9,6 +13,7 @@ export type Props = PrimitiveProps<Property>;
 
 export type Property = {
   default?: {
+    sourceType?: "url" | "OSM";
     tileset?: string;
     styleUrl?: string;
     shadows?: "disabled" | "enabled" | "cast_only" | "receive_only";
@@ -18,7 +23,8 @@ export type Property = {
 export default function Tileset({ layer }: PrimitiveProps<Property>): JSX.Element | null {
   const { viewer } = useCesium();
   const { isVisible, property } = layer ?? {};
-  const { tileset, styleUrl, shadows } = (property as Property | undefined)?.default ?? {};
+  const { sourceType, tileset, styleUrl, shadows } =
+    (property as Property | undefined)?.default ?? {};
   const [style, setStyle] = useState<Cesium3DTileStyle>();
 
   const ref = useCallback(
@@ -41,6 +47,17 @@ export default function Tileset({ layer }: PrimitiveProps<Property>): JSX.Elemen
       setStyle(new Cesium3DTileStyle(await res.json()));
     })();
   }, [styleUrl]);
+  console.log(sourceType);
+  useEffect(() => {
+    const osmTile = createOsmBuildings();
+    if (sourceType == "OSM") {
+      viewer?.scene.primitives.add(osmTile);
+    } else viewer?.scene.primitives.remove(osmTile);
+    // } else {
+    //   osmTile = createOsmBuildings();
+    //   viewer?.scene.primitives.remove(createOsmBuildings());
+    // }
+  }, [isVisible, sourceType, viewer?.scene.primitives]);
 
   return !isVisible || !tileset ? null : (
     <Cesium3DTileset
