99487fb596dc82f5e9b1f57e36943a411c652ace
lby <icesunex@hotmail.com>
2022-11-14 15:59:09 +0800

    refactor: clean up

--- src/components/molecules/Visualizer/hooks.ts
+++ src/components/molecules/Visualizer/hooks.ts
@@ -261,91 +261,88 @@ export default ({
     alignSystem,
   );
 
-  const overrideWidgetPosition = useCallback(
-    (widgetId: string, options: SetUIPositionOptions) => {
-      if (
-        !widgetId ||
-        !["outer", "inner"].includes(options.zone) ||
-        !["left", "center", "right"].includes(options.section) ||
-        !["top", "middle", "bottom"].includes(options.area) ||
-        (options.section === "center" && options.area === "middle")
-      )
-        return;
+  const overrideWidgetPosition = useCallback((widgetId: string, options: SetUIPositionOptions) => {
+    if (
+      !widgetId ||
+      !["outer", "inner"].includes(options.zone) ||
+      !["left", "center", "right"].includes(options.section) ||
+      !["top", "middle", "bottom"].includes(options.area) ||
+      (options.section === "center" && options.area === "middle")
+    )
+      return;
 
-      let tarWidget: Widget | undefined;
+    let tarWidget: Widget | undefined;
 
-      setOverrideAlignSystem(alignSystem => {
-        if (!alignSystem) return alignSystem;
-        Object.keys(alignSystem).forEach(zoneName => {
-          const zone = alignSystem[zoneName as keyof WidgetAlignSystem];
-          if (zone) {
-            Object.keys(zone).forEach(sectionName => {
-              const section = zone[sectionName as keyof WidgetZone];
-              if (section) {
-                Object.keys(section).forEach(areaName => {
-                  const area = section[areaName as keyof WidgetSection];
-                  if (!tarWidget && area?.widgets) {
-                    let tarIndex;
-                    for (let i = 0, len = area.widgets.length; i < len; i += 1) {
-                      if (area.widgets[i].id === widgetId) {
-                        tarIndex = i;
-                        break;
-                      }
-                    }
-                    if (tarIndex !== undefined) {
-                      [tarWidget] = area.widgets.splice(tarIndex, 1);
+    setOverrideAlignSystem(alignSystem => {
+      if (!alignSystem) return alignSystem;
+      Object.keys(alignSystem).forEach(zoneName => {
+        const zone = alignSystem[zoneName as keyof WidgetAlignSystem];
+        if (zone) {
+          Object.keys(zone).forEach(sectionName => {
+            const section = zone[sectionName as keyof WidgetZone];
+            if (section) {
+              Object.keys(section).forEach(areaName => {
+                const area = section[areaName as keyof WidgetSection];
+                if (!tarWidget && area?.widgets) {
+                  let tarIndex;
+                  for (let i = 0, len = area.widgets.length; i < len; i += 1) {
+                    if (area.widgets[i].id === widgetId) {
+                      tarIndex = i;
+                      break;
                     }
                   }
-                });
-              }
-            });
-          }
-        });
+                  if (tarIndex !== undefined) {
+                    [tarWidget] = area.widgets.splice(tarIndex, 1);
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
+      return { ...alignSystem };
+    });
+
+    setTimeout(() => {
+      setOverrideAlignSystem(alignSystem => {
+        if (!alignSystem || !tarWidget) return alignSystem;
+        if (!alignSystem[options.zone]) {
+          alignSystem[options.zone] = {
+            left: undefined,
+            center: undefined,
+            right: undefined,
+          };
+        }
+
+        const tarZone = alignSystem[options.zone] as WidgetZone;
+        if (!tarZone[options.section]) {
+          tarZone[options.section] = {
+            top: undefined,
+            middle: undefined,
+            bottom: undefined,
+          };
+        }
+
+        const tarSection = tarZone[options.section] as WidgetSection;
+        if (!tarSection[options.area]) {
+          tarSection[options.area] = {
+            align: "start",
+            widgets: [],
+          };
+        }
+
+        const tarArea = tarSection[options.area] as WidgetArea;
+        if (!tarArea.widgets) tarArea.widgets = [];
+        if (options.method === "insert") {
+          tarArea.widgets.unshift(tarWidget);
+        } else {
+          tarArea.widgets.push(tarWidget);
+        }
+
         return { ...alignSystem };
       });
-
-      setTimeout(() => {
-        setOverrideAlignSystem(alignSystem => {
-          if (!alignSystem || !tarWidget) return alignSystem;
-          if (!alignSystem[options.zone]) {
-            alignSystem[options.zone] = {
-              left: undefined,
-              center: undefined,
-              right: undefined,
-            };
-          }
-
-          const tarZone = alignSystem[options.zone] as WidgetZone;
-          if (!tarZone[options.section]) {
-            tarZone[options.section] = {
-              top: undefined,
-              middle: undefined,
-              bottom: undefined,
-            };
-          }
-
-          const tarSection = tarZone[options.section] as WidgetSection;
-          if (!tarSection[options.area]) {
-            tarSection[options.area] = {
-              align: "start",
-              widgets: [],
-            };
-          }
-
-          const tarArea = tarSection[options.area] as WidgetArea;
-          if (!tarArea.widgets) tarArea.widgets = [];
-          if (options.method === "insert") {
-            tarArea.widgets.unshift(tarWidget);
-          } else {
-            tarArea.widgets.push(tarWidget);
-          }
-
-          return { ...alignSystem };
-        });
-      }, 0);
-    },
-    [overriddenAlignSystem],
-  );
+    }, 0);
+  }, []);
 
   useEffect(() => {
     setOverrideAlignSystem(alignSystem);
