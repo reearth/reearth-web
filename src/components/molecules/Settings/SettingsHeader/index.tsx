a2981835a63f414d15910fe2dcf737a4cf64c5ef
KaWaite <kyle.waite@outlook.com>
2022-11-23 16:17:57 +0900

    add policy to workspace settings page

--- src/components/molecules/Settings/SettingsHeader/index.tsx
+++ src/components/molecules/Settings/SettingsHeader/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Flex from "@reearth/components/atoms/Flex";
 import Text from "@reearth/components/atoms/Text";
 import { useT } from "@reearth/i18n";
 import { styled, useTheme } from "@reearth/theme";
@@ -10,6 +11,10 @@ export type Props = {
   currentWorkspace?: {
     name: string;
     personal?: boolean;
+    policy?: {
+      id: string;
+      name: string;
+    } | null;
   };
   currentProject?: string;
   onChange?: (name: string) => void;
@@ -17,25 +22,42 @@ export type Props = {
 
 const SettingsHeader: React.FC<Props> = ({ title, currentWorkspace, currentProject }) => {
   const t = useT();
-  const workspace = currentWorkspace?.name;
+  const { name: workspaceName, policy } = currentWorkspace ?? {};
   const theme = useTheme();
 
   return (
     <Wrapper>
-      <Text size="xl" color={theme.main.strongText} weight="bold">
-        {workspace} {workspace && (title || currentProject) && " / "}
-        {currentProject} {title && currentProject && " / "}
-        {title}
-      </Text>
-      <Text size="m" color={theme.main.text} otherProperties={{ marginTop: "12px" }}>
-        {currentWorkspace?.personal && t("(Your personal workspace)")}
-      </Text>
+      <Flex gap={12} align="center">
+        <Text size="xl" color={theme.main.strongText} weight="bold">
+          {workspaceName} {workspaceName && (title || currentProject) && " / "}
+          {currentProject} {title && currentProject && " / "}
+          {title}
+        </Text>
+        {policy?.name && !currentProject && !title && (
+          <PolicyText size="m" weight="bold">
+            {policy.name}
+          </PolicyText>
+        )}
+      </Flex>
+      {currentWorkspace?.personal && (
+        <Text size="m" color={theme.main.text} otherProperties={{ marginTop: "12px" }}>
+          {t("(Your personal workspace)")}
+        </Text>
+      )}
     </Wrapper>
   );
 };
+
+export default SettingsHeader;
 
 const Wrapper = styled.div`
   padding: ${metricsSizes["l"]}px 0;
 `;
 
-export default SettingsHeader;
+const PolicyText = styled(Text)`
+  background: #2b2a2f;
+  padding: 4px 20px;
+  border-radius: 12px;
+  user-select: none;
+  transition: background 0.2s;
+`;
