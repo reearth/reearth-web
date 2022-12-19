ec25f2fad954514b4fcef5c67bf9776e3c8ae6e0
keiya01 <keiya.s.0210@gmail.com>
2022-12-19 11:37:50 +0900

    feat: add onLayerEdit property to context

--- src/core/engines/Cesium/Feature/Box/hooks/box.ts
+++ src/core/engines/Cesium/Feature/Box/hooks/box.ts
@@ -10,13 +10,12 @@ import {
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useCesium } from "resium";
 
-// TODO: Use new plugin system
-import { useContext } from "@reearth/components/molecules/Visualizer/Plugin";
 import { FeatureProps } from "@reearth/core/engines/Cesium/Feature";
 import { toColor } from "@reearth/core/mantle";
 
 import { Property } from "..";
 import { sampleTerrainHeightFromCartesian } from "../../../common";
+import { useContext } from "../../context";
 import { EdgeEventCallback } from "../Edge";
 import { PointEventCallback } from "../ScalePoints";
 import { computeMouseMoveAmount, updateTrs } from "../utils";
@@ -210,7 +209,7 @@ export const useHooks = ({
         nextTranslation,
       ) as Cartographic;
 
-      ctx?.emit("layeredit", {
+      ctx?.onLayerEdit?.({
         layerId,
         scale: {
           width: nextScale.x,
@@ -261,7 +260,7 @@ export const useHooks = ({
 
       const { heading, pitch, roll } = HeadingPitchRoll.fromQuaternion(nextRotation);
 
-      ctx?.emit("layeredit", {
+      ctx?.onLayerEdit?.({
         layerId,
         rotate: {
           heading,
