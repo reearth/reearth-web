243f1b6122e9c0a6d56fb6bba831ef0651c47a3c
pyshx <piyushchauhan1004@gmail.com>
2023-01-11 13:30:50 +0530

    fix: remove `number` and `boolean` from styleExpression as the support type for expression

--- src/core/mantle/types/expression.ts
+++ src/core/mantle/types/expression.ts
@@ -2,7 +2,7 @@ export type ExpressionContainer = {
   expression: StyleExpression;
 };
 
-export type StyleExpression = ConditionsExpression | string | boolean | number;
+export type StyleExpression = ConditionsExpression | string;
 
 export type ConditionsExpression = {
   conditions: [string, string][];
