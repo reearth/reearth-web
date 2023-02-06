67ba9df31a0f1a79ac3360ad7cc30fe9dd4cd796
pyshx <piyushchauhan1004@gmail.com>
2023-02-06 12:27:55 +0530

    fix: ci error

--- src/core/mantle/data/index.ts
+++ src/core/mantle/data/index.ts
@@ -2,14 +2,14 @@ import type { Data, DataRange, Feature } from "../types";
 
 import { fetchCSV } from "./csv";
 import { fetchGeoJSON } from "./geojson";
-import { useFetchGTFS } from "./gtfs";
+import { fetchGTFS } from "./gtfs";
 
 export type DataFetcher = (data: Data, range?: DataRange) => Promise<Feature[] | void>;
 
 const registry: Record<string, DataFetcher> = {
   geojson: fetchGeoJSON,
   csv: fetchCSV,
-  gtfs: useFetchGTFS,
+  gtfs: fetchGTFS,
 };
 
 export async function fetchData(data: Data, range?: DataRange): Promise<Feature[] | void> {
