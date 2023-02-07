9084aa0b95b7c4a7e02c5ae25d8df192ed47a6b4
basel.issmail <basel.issmail@gmail.com>
2023-02-07 17:38:18 +0300

    default gap value

--- src/core/Crust/Widgets/WidgetAlignSystem/MobileZone.tsx
+++ src/core/Crust/Widgets/WidgetAlignSystem/MobileZone.tsx
@@ -66,7 +66,7 @@ export default function MobileZone({
                   widgets={zone?.[s]?.[a]?.widgets}
                   align={zone?.[s]?.[a]?.align ?? "start"}
                   padding={zone?.[s]?.[a]?.padding ?? { top: 0, bottom: 0, left: 0, right: 0 }}
-                  gap={zone?.[s]?.[a]?.gap ?? 0}
+                  gap={zone?.[s]?.[a]?.gap ?? 6}
                   centered={zone?.[s]?.[a]?.centered ?? false}
                   backgroundColor={zone?.[s]?.[a]?.background ?? "unset"}
                   layoutConstraint={layoutConstraint}
--- src/core/Crust/Widgets/WidgetAlignSystem/Zone.tsx
+++ src/core/Crust/Widgets/WidgetAlignSystem/Zone.tsx
@@ -49,7 +49,7 @@ export default function Zone({
                 align={zone?.[s]?.[a]?.align ?? "start"}
                 padding={zone?.[s]?.[a]?.padding ?? { top: 0, bottom: 0, left: 0, right: 0 }}
                 backgroundColor={zone?.[s]?.[a]?.background ?? "unset"}
-                gap={zone?.[s]?.[a]?.gap ?? 0}
+                gap={zone?.[s]?.[a]?.gap ?? 6}
                 centered={zone?.[s]?.[a]?.centered ?? false}
                 layoutConstraint={layoutConstraint}
                 renderWidget={renderWidget}
