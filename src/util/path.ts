0996168bf428360dca2eee9bb3bc6f91113d9d72
keiya sasaki <keiya.s.0210@gmail.com>
2023-02-07 20:04:37 +0900

    Update src/util/path.ts
    Co-authored-by: rot1024 <aayhrot@gmail.com>

--- src/util/path.ts
+++ src/util/path.ts
@@ -11,5 +11,10 @@ export const parseHost = (source: DataSource): string | undefined => {
   return parsed[2];
 };
 
-export const getExtname = (url: string | undefined): string | undefined =>
-  url?.match(/\.([a-z]+?)(?:\?.*?)?$/)?.[1];
+export const getExtname = (url: string | undefined): string | undefined => {
+  try {
+    return new URL(url).pathname.split(".")[1];
+  } catch {
+    return undefined;
+  }
+};
